21a4b116dca0a2af38d9da3d56c7f146ec759917
Heng Cao <hengcao@macbookair.home>
2025-08-10 23:20:44 -0400

    Ignore TS build errors on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Don’t fail the production build on ESLint errors
-    ignoreDuringBuilds: true
-  }
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
 };
 
 export default nextConfig;
